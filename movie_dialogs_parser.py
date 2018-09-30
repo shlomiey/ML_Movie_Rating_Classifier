3c6695db65c1ca538e6f8960e9968b47d271790d
shlomiey@campus.technion.ac.il <nissim10>
2018-09-30 20:42:55 +0300

    Final Update

--- movie_dialogs_parser.py
+++ movie_dialogs_parser.py
@@ -171,6 +171,7 @@ class MovieDialogParser:
     Bonus2: added a rating2ID_dictionary for easy
                 IMDB rating (float) -> [movie_id_1, movie_id_2, ...] mapping
     """
+
     def __init__(self):
         self.movie_lines = movie_lines_to_dict()
         self.movie_characters = movie_characters_metadata_to_dict()
@@ -222,13 +223,54 @@ class MovieDialogParser:
                     inner_dict['total_lines'] = total_movie_lines
             inner_dict['total_conversations'] = len(conversation_dic)
             self.corpus_dictionary[movie_id] = inner_dict
-            
+
             # updating the rating2ID dictionary
             movie_rating = movie_metadata_dic[movie_id]['IMDB rating']
             if movie_rating in self.rating2ID_dictionary.keys():
                 self.rating2ID_dictionary[movie_rating].append(movie_id)
             else:
                 self.rating2ID_dictionary[movie_rating] = [movie_id]
+
+
+def viterbi_algorithm(obs, states, start_p, trans_p, emit_p):
+    vertices = [{}]
+    for st in states:
+        vertices[0][st] = {"prob": start_p[st] * emit_p[st][obs[0]], "prev": None}
+    # Run Viterbi when t > 0
+    for t in range(1, len(obs)):
+        vertices.append({})
+        for st in states:
+            max_tr_prob = max(vertices[t - 1][prev_st]["prob"] * trans_p[prev_st][st] for prev_st in states)
+            for prev_st in states:
+                if vertices[t - 1][prev_st]["prob"] * trans_p[prev_st][st] == max_tr_prob:
+                    max_prob = max_tr_prob * emit_p[st][obs[t]]
+                    vertices[t][st] = {"prob": max_prob, "prev": prev_st}
+                    break
+    for line in dict_table(vertices):
+        print(line)
+    opt = []
+    # The highest probability
+    max_prob = max(value["prob"] for value in vertices[-1].values())
+    previous = None
+    # Get most probable state and its backtrack
+    for st, data in vertices[-1].items():
+        if data["prob"] == max_prob:
+            opt.append(st)
+            previous = st
+            break
+    # Follow the backtrack till the first observation
+    for t in range(len(vertices) - 2, -1, -1):
+        opt.insert(0, vertices[t + 1][previous]["prev"])
+        previous = vertices[t + 1][previous]["prev"]
+
+    print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+
+
+def dict_table(dict):
+    # Print a table of steps from dictionary
+    yield " ".join(("%12d" % i) for i in range(len(dict)))
+    for state in dict[0]:
+        yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
 
 # for debugging & example purposes
